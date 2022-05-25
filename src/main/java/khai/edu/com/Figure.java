8cf3a9e8dda1163b6174ebfe25400b1b3810fe79
Admin <alexanderorlovkh@gmail.com>
2022-05-25 09:09:50 +0300

    I hope this is the last commit

--- src/main/java/khai/edu/com/Figure.java
+++ src/main/java/khai/edu/com/Figure.java
@@ -1,7 +1,6 @@
 package khai.edu.com;
 
 abstract class Figure{
-// бтв я хз че ещё можно закинуть сюда, переменные же у всех разные будут, остаются only methods
 public abstract double findArea();
 public abstract double findGravityCenter();
 public abstract double findPerimeter();
